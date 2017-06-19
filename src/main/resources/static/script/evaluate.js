e63a8bf40f34101bf5fd000bde7cf327f5c04102
Sean O'Connor <soconnor@yahoo.com>
2017-06-19 21:49:29 +0800

    result field

--- src/main/resources/static/script/evaluate.js
+++ src/main/resources/static/script/evaluate.js
@@ -1,12 +1,13 @@
 
 evaluateExpression = function() {
-    $("#errorMsg").text("")
+    $("#errorMsg").text("");
+    $("#result").text("");
 
     var exprText = $.trim($("#expression").val());
     var json = $.trim($("#json").val());
 
     if (exprText === "" || json === "") {
-        $("#errorMsg").text("Provide expression and json values.");
+        $("#errorMsg").text("Provide expression and JSON values.");
         return;
     }
 
