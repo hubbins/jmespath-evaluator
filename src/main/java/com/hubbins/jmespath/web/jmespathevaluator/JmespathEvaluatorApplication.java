01d0aa7442111594857217d32d2cd52dd3f7ebdc
Sean O'Connor <soconnor@yahoo.com>
2017-06-20 21:33:35 +0800

    change to war file

--- src/main/java/com/hubbins/jmespath/web/jmespathevaluator/JmespathEvaluatorApplication.java
+++ src/main/java/com/hubbins/jmespath/web/jmespathevaluator/JmespathEvaluatorApplication.java
@@ -2,9 +2,16 @@ package com.hubbins.jmespath.web.jmespathevaluator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JmespathEvaluatorApplication {
+public class JmespathEvaluatorApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JmespathEvaluatorApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JmespathEvaluatorApplication.class, args);
